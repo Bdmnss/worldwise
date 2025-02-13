cf87a4f84c97e2a6367f3bf8ce5037a676ed7cff
Luka Badali <150358418+Bdmnss@users.noreply.github.com>
2025-02-13 22:41:55 +0400

    Fix Styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
-import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
@@ -19,7 +18,6 @@ function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
-          <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />}>
             <Route index element={<Navigate replace to="cities" />} />
             <Route path="cities" element={<CityList />} />
